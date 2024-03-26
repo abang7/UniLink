49fe69f0845c501127cbe95449dbc332f02372c3
bang <947928577@qq.com>
2024-03-27 02:15:14 +0800

    v1

---
+++ UniLink-server/src/test/java/com/bang/unilinkserver/UniLinkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bang.unilinkserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniLinkServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
